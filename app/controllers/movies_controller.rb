79bf71e139f8dc1ff2bc7f3a4ffb2180df9ab174
radek <kedar03@gmail.com>
2017-07-10 17:39:30 +0200

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,20 +10,47 @@ class MoviesController < ApplicationController
     # will render app/views/movies/show.<extension> by default
   end
 
+
   def index
+
+    query_parameters = {}
+    redirect_flag = false
     @all_ratings = Movie.all_ratings
+
     if params[:ratings].nil?
-      @selected_ratings = @all_ratings
+      if session[:selected_ratings].nil?
+        session[:selected_ratings] = @all_ratings.map { |r| [r, '1'] }.to_h
+      else
+        @selected_ratings = session[:selected_ratings].keys
+      end
+      query_parameters[:ratings] = session[:selected_ratings]
+      redirect_flag = true
     else
       @selected_ratings = params[:ratings].keys
+      session[:selected_ratings] = params[:ratings]
+      query_parameters[:ratings] = session[:selected_ratings]
     end
+
     @movies = Movie.where(rating: @selected_ratings)
+
     if params[:sort_by] == 'title'
-       @movies.order!(title: :asc)
-       @sort_by = :title
+      @sort_by = :title
+      session[:sort_by] = @sort_by
     elsif params[:sort_by] == 'release_date'
-      @movies.order!(release_date: :asc)
-      @sort_by = :release_date
+      @sort_by = :release_date     
+      session[:sort_by] = @sort_by
+    elsif not session[:sort_by].nil?
+      redirect_flag = true
+      query_parameters[:sort_by] = session[:sort_by]
+    end
+
+    if redirect_flag == true
+      flash.keep
+      redirect_to movies_path(query_parameters)
+    end
+
+    if not @sort_by.nil?
+      @movies.order!("#{@sort_by} ASC")
     end
   end
 
